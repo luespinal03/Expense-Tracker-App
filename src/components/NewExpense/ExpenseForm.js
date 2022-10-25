eb49c2d7f88c53d6ce37a738b1dc2fd0442a8900
Luis Espinal <luisespinal294@gmail.com>
2022-10-25 14:21:31 -0400

    testing something out

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,11 +1,14 @@
 import './ExpenseForm.css'
 import { useState } from 'react';
 
-const ExpenseForm = () => {
+// Here we are passing onSaveExpenseDataHandler as a props. Function in charge of spreading the current information that was collected based on user input, adding an ID to our newly created expense
+const ExpenseForm = (props) => {
+    console.log(props);
 
     const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAmount, setEnteredAmount] = useState('')
+    const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setenteredDate] = useState('');
+
 
     // handling the title based on what the user inputs
     const titleChangeHandler = (e) => {
@@ -23,31 +26,53 @@ const ExpenseForm = () => {
     const dateChangeHandler = (e) => {
         // console.log(e);
         setenteredDate(e.target.value);
+    }
 
+    // prevent default from the event (full page refresh)
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        // creating a new expense based on user input data
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+
+        // here we are passing our user collected data back up to onSaveExpenseData
+        props.onSaveExpenseData(expenseData);
+
+        // 3 lines of code below this comment are to reset the input values to empty after the form gets submitted
+        setEnteredTitle('')
+        setEnteredAmount('')
+        setenteredDate('')
     }
 
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler} />
+                    {/* value is what the input will begin with, in this case it will start off as empty */}
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
                 </div>
             </div>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+                    {/* value is what the input will begin with, in this case it will start off as empty */}
+                    <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler} />
                 </div>
             </div>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2023-12-31" onChange={dateChangeHandler} />
+                    {/* value is what the input will begin with, in this case it will start off as empty */}
+                    <input type="date" min="2019-01-01" max="2023-12-31" value={enteredDate} onChange={dateChangeHandler} />
                 </div>
             </div>
             <div className="new-expense__actions">
-                <button type="submit">Submit</button>
+                <button type="submit">Add Expense</button>
             </div>
         </form>
     );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,9 +3,21 @@ import ExpenseForm from './ExpenseForm';
 
 
 const NewExpense = () => {
+
+    // function below is handling the new created data, being passed in up from ExpenseForm component
+    const onSaveExpenseDataHandler = (enteredExpenseData) => {
+        // here we are spreading the data object we collected from our user and adding it a random ID
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString() * 1000
+        };
+        console.log(expenseData);
+    };
+
     return (
         <div className='new-expense'>
-            <ExpenseForm />
+            {/* naming convention, start with on so we know the value for this prop is a function that can be called inside the ExpenseForm component */}
+            <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
         </div>
     )
 }
